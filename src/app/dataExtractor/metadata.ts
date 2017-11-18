1c8452c784d374bee594b7089f3be653b2d7c8d4
lucy-i <lucy@ilucy.in>
2017-11-18 09:28:03 +0530

    updated code with property edit section

--- src/app/dataExtractor/metadata.ts
+++ src/app/dataExtractor/metadata.ts
@@ -16,4 +16,23 @@ export class LoanMetadata {
             { value: "06", text: "GPM" },
             { value: "13", text: "Other" }];
     }
+    get PuposeOfLoan() {
+        return [{ value: "04", text: "Construction" },
+        { value: "05", text: "Refinance" },
+        { value: "13", text: "Construction-Perm" },
+        { value: "15", text: "Other" },
+        { value: "16", text: "Purchase" }];
+    }
+    get ResidenceType() {
+        return [{ value: "1", text: "Primary Residence" },
+        { value: "2", text: "Secondary Residence" },
+        { value: "D", text: "Investment Property" }
+        ];
+    }
+    get MartialStatus(){
+        return [{ value: "M",text:"Married"},
+                { value: "S",text:"Separated"},
+                { value: "U",text:"Unmarried (Single, Divorced, Widowed)"}
+                ];
+    }
 }
